f5bb48aebb6197378c2ac7c5ec56de8b97728e51
alkaabi1972 <alkaabi1972@gmail.com>
2026-01-01 14:11:24 -0700

    init

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,20 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+vim.cmd.colorscheme 'vim'
+-- Set for all modes with blinking
+vim.opt.guicursor = 'a:blinkwait700-blinkon400-blinkoff250'
+vim.cmd [[highlight Cursor guibg=#FFFF00 guifg=#FFFFFF]]
+-- Enable cursorline
+vim.opt.cursorline = false
+
+-- Change the background color of the current line (guibg for GUI, ctermbg for terminal)
+vim.api.nvim_set_hl(100, 'CursorLine', { bg = '#000000', blend = 100 })
+-- You can use hex codes or standard color names
+
+-- Optionally, change the color of the current line number
+vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#AABBCC', blend = 50 })
+-- vim: ts=2 sts=2 sw=2 et
+return {}
